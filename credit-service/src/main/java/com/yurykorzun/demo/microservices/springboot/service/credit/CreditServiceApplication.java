5e4f01570afd9201a02323642e970f37e2ddfb79
KorzunYuri <korzun.yuri@gmail.com>
2023-10-22 21:38:29 +0200

    credit-service: init

---
+++ credit-service/src/main/java/com/yurykorzun/demo/microservices/springboot/service/credit/CreditServiceApplication.java
@@ -0,0 +1,12 @@
+package com.yurykorzun.demo.microservices.springboot.service.credit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditServiceApplication.class, args);
+    }
+}
